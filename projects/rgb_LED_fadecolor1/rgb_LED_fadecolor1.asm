5af8d38fa4e6b88804939b171bd65757121235b7
Ethan Durrant <emd519@yahoo.com>
2014-08-22 23:07:32 -0700

    adding and modifying code to do rgb fading

--- projects/rgb_LED_fadecolor1/rgb_LED_fadecolor1.asm
+++ projects/rgb_LED_fadecolor1/rgb_LED_fadecolor1.asm
@@ -1,4 +1,4 @@
-;this program will do PWM on an LED connected to PB0 
+;this program will do PWM to fade RGB led on PB2-4
 ;on an STK500 board.
 ;the PWM will be done without the usual capture/compare method b/c the 
 ;attiny11's one 8-bit timer doesn't have that ability, only timer overflow.
@@ -24,6 +24,12 @@
 
 .def PWM_INC_VAL = r13; to store increments to PWM in a counter
 
+.def LED_RGB_SEL = r24; to store which led is avtivated
+.equ LED_R_bit = 2;R
+.equ LED_G_bit = 1;G
+.equ LED_B_bit = 0;B
+
+
 .equ PWM_dir_bit = 1; bit for PWM direction status in status_regA
 .equ PWM_flag_bit = 2;
 
@@ -34,6 +40,15 @@
 	eor tgl_io_regA, tgl_io_regB; toggle the bit in other reg
 	out @0, tgl_io_regA; send it back to the io PORTx in 1st arg
 .endmacro
+
+;.macro tgl_io_byte
+;;
+;	in tgl_io_regA, @0; input the io data in first arg
+;	ldi  tgl_io_regB, @1; input the 2nd arg bit to reg b
+;	eor tgl_io_regA, tgl_io_regB; toggle the bit in other reg
+;	out @0, tgl_io_regA; send it back to the io PORTx in 1st arg
+;
+;.endmacro
 
 .macro tgl_tfr_reg
     ldi transfer_regA, (1<<@1);toggle a bit in a register using
@@ -53,7 +68,15 @@
     RJMP TIM0_OVF   ;Timer0 Overflow Handler
 
 ON_RESET:
-    SBI DDRB,PB0        ;SET PORTB0 FOR OUTPUT by setting bit 0 in DDRB
+    ;SBI DDRB,PB0        ;SET PORTB0 FOR OUTPUT by setting bit 0 in DDRB
+    ldi A,0b00011100;set PB2-4 to output
+    out DDRB,A; load into DDRB
+    ldi LED_RGB_SEL,0b00000101; set to start with Red (bit2)
+    
+    ori status_regA, (1<<PWM_dir_bit);//set the direction to always be up
+    
+    tgl_io PORTB,3;
+    
     LDI A,0b00000011    ;SET PRESCALER TO /64      
     OUT TCCR0,A         ;load into TIMER/COUNTER CONTROL REGISTER 
     LDI A,0b00000010    ;ENABLE TIMER-OVERFLOW INTERUPT
@@ -71,8 +94,12 @@ MAIN_LOOP:
 ; TIMER OVER-FLOW INTERUPT ROUTINE ;
 ;----------------------------------;
 TIM0_OVF:
-    tgl_io   PORTB,0       ;FLIP THE 0 BIT in PORTB to toggle PBO
-      
+    
+    
+    ;;tgl_io   PORTB,0       ;FLIP THE 0 BIT in PORTB to toggle PBO
+    
+    rcall WHICH_TOGGLE;
+    
     rcall PWM_INC; call the PWM_INC subroutine to change PWM val
 
     ldi pwm_calc_store,255; reset to 255
@@ -84,7 +111,41 @@ TIM0_OVF:
         out TCNT0,pwm_val_store; set counter  
     
     RETI; return from the interrupt service routine
- 
+
+SWITCH_LED:
+    clr A;
+    sbrc LED_RGB_SEL,LED_R_bit; skip if clear
+        ldi A, 0b00000010;set to G in A
+    sbrc LED_RGB_SEL,LED_G_bit; skip if clear
+        ldi A, 0b00000001;set to B
+    sbrc LED_RGB_SEL,LED_B_bit; skip if clear
+        ldi A, 0b00000100;set to R
+        
+    mov LED_RGB_SEL,A; put reg A into other reg
+    clr A;
+    cbr status_regA, (1<<PWM_flag_bit); clear the PWM_flag_bit
+    
+    rcall WHICH_TOGGLE;
+    
+    ret;
+    
+WHICH_TOGGLE:
+    clr A;
+    sbrc LED_RGB_SEL,LED_R_bit;
+        ldi A,0b00010100;
+    sbrc LED_RGB_SEL,LED_G_bit;
+        ldi A,0b00011000;
+    sbrc LED_RGB_SEL,LED_B_bit;
+        ldi A,0b00001100;
+    
+    in tgl_io_regA,PORTB;
+    eor tgl_io_regA,A; xor toggle the bits
+    out PORTB, tgl_io_regA; put to port
+    
+    clr A;
+    ret;
+
+
 PWM_INC:
     sbrs status_regA,PWM_dir_bit; skip if set
         rcall put_pwm_down; call the put_pwm_dowm subroutine
@@ -92,7 +153,8 @@ PWM_INC:
         rcall put_pwm_up; call the put_pwm_up subroutine
     
     sbrc status_regA,PWM_flag_bit; skip if flag is clear
-        rcall TOGGLE_PWM_STATUS; call the TOGGLE_PWM_STATUS subroutine
+        ;rcall TOGGLE_PWM_STATUS; call the TOGGLE_PWM_STATUS subroutine
+        rcall SWITCH_LED; switch led
     RET; return from subroutine
 
 TOGGLE_PWM_STATUS: ;toggles the PWM change direction (up or down)
